951df66fd98d4d406964794b5f357565d56f03d6
Alejandro andres jimenez perales <alejandro_jim32@hotmail.com>
2023-04-24 20:35:43 -0300

    [web] Agregar Niveles y componetizacion de las funciones de Javascript

--- src/js/game.js
+++ src/js/game.js
@@ -1,45 +1,62 @@
 const sceneEl = document.querySelector("a-scene");
 const limonEL = document.querySelector("#limon-model");
 const scoreEl = document.querySelector("#score-element");
+const posicionesRandomEl = document.querySelector("#posicionesRandom");
 
-let score = 0;
+AFRAME.registerComponent("posiciones-random", {
+  init: function () {
+    //Posiciones random de los limones
+    this.randomPosition = function () {
+      return {
+        x: (Math.random() - 0.5) * 200,
+        y: 1.5,
+        z: (Math.random() - 0.5) * 200,
+      };
+    };
+  },
+});
 
-//set and show new score
-function displayScore() {
-  scoreEl.setAttribute("value", `Score: ${score}`);
-}
+AFRAME.registerComponent("cambiar-entorno", {
+  init: function () {
+    let score = 0;
 
-//random position of lemon
-function randomPosition() {
-  return {
-    x: (Math.random() - 0.5) * 200,
-    y: 1.5,
-    z: (Math.random() - 0.5) * 200,
-  };
-}
+    //obtenemos el componente de posiciones
+    const componentePR = posicionesRandomEl.components["posiciones-random"];
 
-function createLemon() {
-  debugger;
-  const clone = limonEL.cloneNode();
-  clone.setAttribute("position", randomPosition());
-  clone.addEventListener("mousedown", () => {
-    score++;
-    clone.dispatchEvent(new Event("collected"));
+    //set and show new score
+    function displayScore() {
+      scoreEl.setAttribute("value", `Score: ${score}`);
+    }
+
+    function createLemon() {
+      const clone = limonEL.cloneNode();
+      clone.setAttribute("position", componentePR.randomPosition());
+      clone.addEventListener("mousedown", () => {
+        score++;
+        clone.dispatchEvent(new Event("collected"));
+        displayScore();
+        cambiarEntorno();
+      });
+      clone.addEventListener("animationcomplete", () => {
+        clone.setAttribute("position", componentePR.randomPosition());
+        clone.setAttribute("scale", "0.05 0.05 0.05");
+      });
+
+      sceneEl.appendChild(clone);
+    }
+
+    for (let i = 0; i < 15; i++) {
+      createLemon();
+    }
+
+    // Obtener la escena de A-Frame
+    const escena = sceneEl;
+    // Al encontrar n limones, se cambiara a un siguiente ambiente (Nivel)
+    function cambiarEntorno() {
+      if (score === 2) {
+        escena.setAttribute("environment", "preset: egypt");
+      }
+    }
     displayScore();
-  });
-  clone.addEventListener("animationcomplete", () => {
-    clone.setAttribute("position", randomPosition());
-    clone.setAttribute("scale", "0.05 0.05 0.05");
-  });
-
-  sceneEl.appendChild(clone);
-  if (score == 10) {
-    sceneEl.setAttribute("enviroment", "preset:egypt");
-  }
-}
-
-for (let i = 0; i < 15; i++) {
-  createLemon();
-}
-
-displayScore();
+  },
+});
